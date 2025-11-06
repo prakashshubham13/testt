9f52bc11b93831b02bdb637317f1b4bab33e40ba
shubham prakash <60683584+prakashshubham13@users.noreply.github.com>
2025-11-06 10:52:19 +0530

    Create HostedCheckoutRepository.java

---
+++ HostedCheckoutRepository.java
@@ -0,0 +1,16 @@
+package com.welhire.welhire_subscription_service.repository;
+
+import com.welhire.welhire_subscription_service.entity.HostedCheckout;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface HostedCheckoutRepository extends JpaRepository<HostedCheckout, Long> {
+    Optional<HostedCheckout> findByOrderId(String orderId);
+    Optional<HostedCheckout> findByProviderHostedpageId(String hostedpageId);
+    Optional<HostedCheckout> findFirstByProviderHostedpageId(String providerHostedpageId);
+    Optional<HostedCheckout> findFirstByProviderDecryptedHostedpageId(String providerDecryptedHostedpageId);
+    List<HostedCheckout> findAllByTenantIdAndBillingIdOrderByCreatedAtDesc(String tenantId, String billingId);
+
+}
